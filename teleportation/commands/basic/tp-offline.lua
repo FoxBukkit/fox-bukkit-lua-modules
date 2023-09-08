5ea16b766cc3e9f0bd318b86d64986721ecdeda8
Doridian <git@doridian.net>
2023-09-07 21:05:27 -0700

    formatting

---
+++ teleportation/commands/basic/tp-offline.lua
@@ -0,0 +1,60 @@
+local Command = require('Command')
+local Permission = require('Permission')
+local Locationstack = require('Locationstack')
+local Chat = require('Chat')
+local Player = require('Player')
+
+local bukkitServer = require('Server'):getBukkitServer()
+
+local Event = require('Event')
+
+Event:register{
+	class = 'org.bukkit.event.player.PlayerQuitEvent',
+	priority = Event.Priority.MONITOR,
+	ignoreCancelled = true,
+	run = function(_, event)
+		local ply = Player:extend(event:getPlayer())
+		ply.logoutLocation = ply:getLocation()
+	end,
+}
+
+Command:register{
+	name = 'tp-offline',
+	action = {
+		format = '%s teleported to %s%s',
+		isProperty = false,
+	},
+	permissionOther = false,
+	arguments = { {
+		name = 'name',
+		type = 'string',
+		required = true,
+	} },
+	run = function(self, ply, args)
+		if not ply:hasPermission(self.permission .. '.offline') then
+			ply:sendError('Permission denied')
+			return
+		end
+
+		local offlineUUID = Chat:getPlayerUUID(args.name)
+		if not offlineUUID then
+			ply:sendError('Player not found')
+			return
+		end
+
+		if not ply:fitsImmunityRequirement(offlineUUID, Permission.Immunity.GREATER_OR_EQUAL) then
+			ply:sendError('Permission denied for target')
+			return
+		end
+
+		local offlinePlayer = Player:extend(bukkitServer:getOfflinePlayer(offlineUUID))
+		if not offlinePlayer.logoutLocation then
+			ply:sendError('Logout location not yet tracked')
+			return
+		end
+
+		Locationstack:add(ply)
+		ply:teleport(offlinePlayer.logoutLocation)
+		self:sendActionReply(ply, offlinePlayer, { silentToTarget = true }, ' (last logout location)')
+	end,
+}
